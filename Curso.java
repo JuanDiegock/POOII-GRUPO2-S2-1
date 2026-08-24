9d916f8d5f665d0a33fc009f2a39b1b965825437
JuanDiego <138148886+JuanDiegock@users.noreply.github.com>
2026-08-24 16:43:14 -0500

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -1,19 +1,13 @@
 public class Curso {
-
     private String nombreCurso;
     private int nota;
 
     public Curso(String nombreCurso) {
         this.nombreCurso = nombreCurso;
-        this.nota = 0;
     }
 
     public String getNombreCurso() {
         return nombreCurso;
-    }
-
-    public void setNombreCurso(String nombreCurso) {
-        this.nombreCurso = nombreCurso;
     }
 
     public int getNota() {
@@ -22,11 +16,5 @@ public class Curso {
 
     public void setNota(int nota) {
         this.nota = nota;
-    }
-
-    @Override
-    public String toString() {
-        return "Curso: " + nombreCurso +
-               " | Nota: " + nota;
     }
 }
